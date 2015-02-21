ed662bd7b1e005d4d1cd8b54ff9692b55fcd63a3
bmcorser <bmcorser@gmail.com>
2015-02-21 13:55:42 +0000

    All the infos

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import sensors
 
+def getcallattr(obj, name):
+    attr = getattr(obj, name)
+    if not callable(attr):
+        return attr
+    return attr()
+
 sensors.init()
 try:
     for chip in sensors.iter_detected_chips():
+        print('--')
         def is_public(attr_name):
             if attr_name.startswith('_') or attr_name.upper() == attr_name:
                 return False
             return not isinstance(getattr(type(chip), attr_name, None), property)
         for attr in filter(is_public, dir(chip)):
-            print(getattr(chip, attr))
-        # print ('%s at %s' % (chip, chip.adapter_name))
-        # for feature in chip:
-            # print ('  %s: %.2f' % (feature.label, feature.get_value()))
+            print("{0}: {1}".format(attr, getattr(chip, attr)))
+        for feature in chip:
+            for attr in filter(is_public, dir(feature)):
+                print("  {0}: {1}".format(attr, getcallattr(feature, attr)))
 finally:
     sensors.cleanup()
